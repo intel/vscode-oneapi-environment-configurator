e05b6aed3a5174c0dfbde6d4cd6370ed6ecfa93b
alexey.makarevich <amakarev@nnloclsdk-cflnv-005.inn.intel.com>
2020-11-18 17:24:27 +0300

    added makefile parsing

--- src/devFlow.ts
+++ src/devFlow.ts
@@ -1,45 +1,54 @@
 import * as vscode from 'vscode';
 import { execSync } from 'child_process';
+import { existsSync } from 'fs';
 export class DevFlow {
     terminal: vscode.Terminal;
+    env: boolean;
     constructor() {
         this.terminal = vscode.window.activeTerminal ? vscode.window.activeTerminal : vscode.window.createTerminal();
+        this.env = false;
     }
     async setEnviroment(): Promise<void> {
-        await vscode.window.showInformationMessage("Provide path to oneAPI setvars.sh.", 'select').then(async selection => {
-            if (selection === 'select') {
-                const options: vscode.OpenDialogOptions = {
-                    canSelectMany: false,
-                    openLabel: 'Select',
-                    filters: {
-                        'oneAPI setvars file': ['sh'],
-                    }
-                };
-                await vscode.window.showOpenDialog(options).then(async fileUri => {
-                    if (fileUri && fileUri[0]) {
-                        await this.terminal.sendText('source ' + fileUri[0].fsPath);
-                    }
-                });
-            }
-        });
+        if (!this.env) {
+            await vscode.window.showInformationMessage("Provide path to oneAPI setvars.sh.", 'select').then(async selection => {
+                if (selection === 'select') {
+                    const options: vscode.OpenDialogOptions = {
+                        canSelectMany: false,
+                        openLabel: 'Select',
+                        filters: {
+                            'oneAPI setvars file': ['sh'],
+                        }
+                    };
+                    await vscode.window.showOpenDialog(options).then(fileUri => {
+                        if (fileUri && fileUri[0]) {
+                            this.terminal.sendText('source ' + fileUri[0].fsPath);
+                            this.env = true;
+                        }
+                    });
+                }
+            });
+        }
     }
     buildSample(): void {
         const tasks: vscode.QuickPickItem[] = [{ label: 'Run Make' }, { label: 'Get Makefile from Cmake' }];
-        vscode.window.showQuickPick(tasks).then(selection => {
-            if (!selection) {
-                return;
-            }
-            switch (selection.label) {
-                case "Run Make":
-                    this.setEnviroment().then(() => this.runMake());
-                    break;
-                case "Get Makefile from Cmake":
-                    this.setEnviroment().then(() => this.makeFromCmake());
-                    break;
-                default:
-                    break;
-            }
+        this.setEnviroment().then(() => {
+            vscode.window.showQuickPick(tasks).then(async selection => {
+                if (!selection) {
+                    return;
+                }
+                switch (selection.label) {
+                    case "Run Make":
+                        await this.runMake();
+                        break;
+                    case "Get Makefile from Cmake":
+                        await this.makeFromCmake();
+                        break;
+                    default:
+                        break;
+                }
+            });
         });
+
     }
     async makeFromCmake(): Promise<void> {
         this.terminal.sendText(`cd ${vscode.workspace.rootPath}`);
@@ -48,17 +57,20 @@ export class DevFlow {
         this.terminal.sendText("cmake ..");
     }
     async runMake(): Promise<void> {
+        this.terminal.sendText(`cd ${vscode.workspace.rootPath}`);
+        this.terminal.sendText(`[ -d ./build ] && cd build`);
         const targets = this.getMakeTargets();
-        vscode.window.showQuickPick(targets).then(selection => {
+        await vscode.window.showQuickPick(targets).then(selection => {
             if (!selection) {
                 return;
             }
-            this.terminal.sendText(`cd ${vscode.workspace.rootPath}`);
             this.terminal.sendText(`make ${selection}`);
         });
     }
     getMakeTargets(): string[] {
-        const targets = execSync(`make -pRrq : 2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($1 !~ "^[#.]") {print $1}}' | egrep -v '^[^[:alnum:]]' | sort`, { cwd: vscode.workspace.rootPath }).toString().split('\n');
+        let buildPath: string | undefined = existsSync(vscode.workspace.rootPath + '/build') ?
+                                            vscode.workspace.rootPath + '/build' : vscode.workspace.rootPath;
+        const targets = execSync(`make -pRrq : 2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($1 !~ "^[#.]") {print $1}}' | egrep -v '^[^[:alnum:]]' | sort`, { cwd: buildPath }).toString().split('\n');
         targets.pop();
         return targets;
     }
