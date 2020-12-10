11c496b475168e8ef3d630f37a50abbff6bf58a3
Makarevich, Alexey <alexey.makarevich@intel.com>
2020-12-10 05:42:31 -0800

    Enabled CI(package creation)

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as devFlow from './devFlow';
 export function activate(context: vscode.ExtensionContext) {
 	let devFlofData = new devFlow.DevFlow();
 	context.subscriptions.push(vscode.commands.registerCommand('oneapi-devflow.runExtension', () => devFlofData.runExtension()));
-	context.subscriptions.push(vscode.commands.registerCommand('oneapi-devflow.refreshEnvFile', () => devFlofData.refreshEnvFile()));
+	context.subscriptions.push(vscode.commands.registerCommand('oneapi-devflow.refreshEnvFile', () => devFlofData.setEnvironment()));
 }
 
 export function deactivate() {
