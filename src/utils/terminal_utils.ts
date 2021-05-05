2f9be981be15a808cb4deb8900da11a0c13b4815
Bobko, Sergey <sergey.bobko@intel.com>
2021-05-05 07:09:38 -0700

    Removed pwsh version check

--- src/utils/terminal_utils.ts
+++ src/utils/terminal_utils.ts
@@ -15,29 +15,3 @@ export async function checkExistingTerminals(): Promise<boolean | undefined> {
     };
     return true;
 }
-
-export function isTerminalAcceptable(): boolean {
-    if (process.platform === 'win32') {
-        if (!isPowerShellVersionAcceptable()) {
-            vscode.window.showErrorMessage("The PowerShell terminal does not meet the requirements. It must be version 7 or higher.");
-            return false;
-        }
-    }
-    return true;
-}
-
-function isPowerShellVersionAcceptable(): boolean {
-    try {
-        const out = execSync(`pwsh -Command "(get-host).version.Major"`).toString();
-        const psVersion = parseInt(out);
-        if (isNaN(psVersion)) {
-            return false;
-        } else {
-            return psVersion >= 7 ? true : false;
-        };
-    }
-    catch (err) {
-        console.error(err);
-        return false;
-    }
-};
