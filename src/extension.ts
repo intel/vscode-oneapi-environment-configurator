baa41e2f2761c1a5d35fa929c0c11f6866bdedbf
Alexey Makarevich <alexey.makarevich@intel.com>
2021-06-02 13:47:46 +0300

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -11,8 +11,8 @@ import { DevFlow } from './devFlow';
 
 export function activate(context: vscode.ExtensionContext): void {
 	const devFlof = new DevFlow(context);
-	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.initializeEnvironment', () => devFlof.environment.initializeDefaultEnvironment));
-	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.initializeEnvironmentConfig', () => devFlof.environment.initializeCustomEnvironment));
+	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.initializeEnvironment', () => devFlof.environment.initializeDefaultEnvironment()));
+	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.initializeEnvironmentConfig', () => devFlof.environment.initializeCustomEnvironment()));
 	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.clearEnvironment', () => devFlof.environment.clearEnvironment()));
 	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.switchEnv', () => devFlof.environment.switchEnv()));
 }
