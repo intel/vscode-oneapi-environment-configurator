5ce706839d2a0891fcdeb946b91bbbb073d98e0c
Sergey Bobko <sergey.bobko@intel.com>
2021-06-09 18:25:04 +0300

    Added parsing from param from setting.json

--- src/devFlow.ts
+++ src/devFlow.ts
@@ -13,5 +13,14 @@ export class DevFlow {
     environment: OneApiEnv;
     constructor(c: vscode.ExtensionContext) {
         this.environment = vscode.workspace.workspaceFile !== undefined ? new MultiRootEnv(c) : new SingleRootEnv(c);
+        const config = vscode.workspace.getConfiguration(
+            "intel-corporation.oneapi-environment-variables",
+          );
+        this.setvarsConfigsPaths = config.get<string[][]>("SETVARS_CONFIG");
+        this.suppressFindSetvars = config.get<boolean>("ONEAPI_ROOT-default");
+        this.oneAPIRootPaths = config.get<string[][]>("ONEAPI_ROOT");
     }
+    public readonly setvarsConfigsPaths: string[][] | undefined;
+    public readonly suppressFindSetvars: boolean | undefined;
+    public readonly oneAPIRootPaths: string[][] | undefined;
 }
