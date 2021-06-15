abc3dc13fcacd59a1814e2cb48da4fd36759e93d
Sergey Bobko <sergey.bobko@intel.com>
2021-06-15 19:15:11 +0300

    Added register function

--- src/devFlow.ts
+++ src/devFlow.ts
@@ -11,16 +11,24 @@ import { OneApiEnv, SingleRootEnv, MultiRootEnv } from './environment';
 
 export class DevFlow {
     environment: OneApiEnv;
-    constructor(c: vscode.ExtensionContext) {
-        this.environment = vscode.workspace.workspaceFile !== undefined ? new MultiRootEnv(c) : new SingleRootEnv(c);
-        const config = vscode.workspace.getConfiguration(
-            "intel-corporation.oneapi-environment-variables",
-          );
-        this.setvarsConfigsPaths = config.get<string[][]>("SETVARS_CONFIG");
-        this.suppressFindSetvars = config.get<boolean>("ONEAPI_ROOT-default");
-        this.oneAPIRootPaths = config.get<string[][]>("ONEAPI_ROOT");
+    private static setvarsConfigsPaths: string[][] | undefined;
+    private static oneAPIRootPath: string | undefined;
+    constructor(context: vscode.ExtensionContext) {
+        this.environment = vscode.workspace.workspaceFile !== undefined ? new MultiRootEnv(context) : new SingleRootEnv(context);
+        DevFlow.register(context, this.environment);
     }
-    public readonly setvarsConfigsPaths: string[][] | undefined;
-    public readonly suppressFindSetvars: boolean | undefined;
-    public readonly oneAPIRootPaths: string[][] | undefined;
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    public static register(context: vscode.ExtensionContext, environment: OneApiEnv) {
+        DevFlow.setvarsConfigsPaths = vscode.workspace.getConfiguration("intel-corporation.oneapi-environment-variables").get<string[][]>('SETVARS_CONFIG');
+        context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
+            if (e.affectsConfiguration('SETVARS_CONFIG')) {
+                DevFlow.setvarsConfigsPaths = vscode.workspace.getConfiguration("intel-corporation.oneapi-environment-variables").get<string[][]>('SETVARS_CONFIG');
+            }
+        }));
+        context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.initializeEnvironment', () => environment.initializeDefaultEnvironment()));
+        context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.initializeEnvironmentConfig', () => environment.initializeCustomEnvironment()));
+        context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.clearEnvironment', () => environment.clearEnvironment()));
+        context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.switchEnv', () => environment.switchEnv()));
+    }
+    
 }
--- src/extension.ts
+++ src/extension.ts
@@ -9,12 +9,9 @@
 import * as vscode from 'vscode';
 import { DevFlow } from './devFlow';
 
+
 export function activate(context: vscode.ExtensionContext): void {
 	const devFlof = new DevFlow(context);
-	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.initializeEnvironment', () => devFlof.environment.initializeDefaultEnvironment()));
-	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.initializeEnvironmentConfig', () => devFlof.environment.initializeCustomEnvironment()));
-	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.clearEnvironment', () => devFlof.environment.clearEnvironment()));
-	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-environment-variables.switchEnv', () => devFlof.environment.switchEnv()));
 }
 
 export function deactivate(): void {
