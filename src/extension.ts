724ae7432ec5d6d35dcd4712f87c582f3246b2e9
Alexey Makarevich <alexey.makarevich@intel.com>
2021-07-22 10:26:00 +0300

    refactored

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,22 @@
  * SPDX-License-Identifier: MIT
  */
 
- 'use strict';
- import * as vscode from 'vscode';
- import { DevFlow } from './devFlow';
- 
- 
- export function activate(context: vscode.ExtensionContext): void {
-	 if ((process.platform === 'win32') || (process.platform === 'linux')) {
-		 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-		 const devFlof = new DevFlow(context);
-	 } else {
-		 vscode.window.showErrorMessage("Failed to activate 'Environment Configurator for Intel oneAPI Toolkits' extension. The extension is only supported on Linux and Windows");
-		 deactivate();
-	 }
- }
- 
- export function deactivate(): void {
-	 console.log("Environment Configurator for Intel oneAPI Toolkits: Goodbye");
- }
- 
+'use strict';
+import * as vscode from 'vscode';
+import { DevFlow } from './devFlow';
+
+
+export function activate(context: vscode.ExtensionContext): void {
+	if ((process.platform !== 'win32') && (process.platform !== 'linux')) {
+		vscode.window.showErrorMessage("Failed to activate 'Environment Configurator for Intel oneAPI Toolkits' extension. The extension is only supported on Linux and Windows");
+		deactivate();
+		return;
+	}
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const devFlof = new DevFlow(context);
+}
+
+export function deactivate(): void {
+	console.log("Environment Configurator for Intel oneAPI Toolkits: Goodbye");
+}
+
