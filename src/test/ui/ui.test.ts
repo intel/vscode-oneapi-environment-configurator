37c6fb5bdbd9e3c5de2594bf3ccd41411d66d460
AKurkina <anastasiia.kurkina@intel.com>
2022-04-06 03:07:20 +0400

    ref: tests update (#69)

--- src/test/ui/ui.test.ts
+++ src/test/ui/ui.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import { Workbench, Notification, WebDriver, VSBrowser, NotificationType, InputBox, ModalDialog } from 'vscode-extension-tester';
 // import { DialogHandler } from 'vscode-extension-tester-native';
 import { expect } from 'chai';
@@ -6,11 +7,18 @@ import { writeFileSync, mkdirSync, rmdirSync } from 'fs';
 
 describe('DevFlow extension UI Tests', function () {
     const samplesPath = join(process.cwd(), 'test-resources', 'samples');
+    let browser: VSBrowser;
     let driver: WebDriver;
+    let workbench: Workbench;
+
+    before(async () => {
+        browser = VSBrowser.instance;
+        driver = browser.driver;
+        workbench = new Workbench();
+      });
+
     before(async () => {
         mkdirSync(samplesPath, { recursive: true });
-        driver = VSBrowser.instance.driver;
-        const workbench = new Workbench();
         await workbench.executeCommand('Intel oneAPI: Clear environment variables');
         await driver.sleep(1000);
     });
@@ -18,7 +26,6 @@ describe('DevFlow extension UI Tests', function () {
     describe('Without workspace', function () {
         describe('Default Initialize', function () {
             it('Quick pick contain command', async function () {
-                const workbench = new Workbench();
                 const input = await workbench.openCommandPrompt() as InputBox;
                 await input.setText('>Intel oneAPI: Initialize default environment variables');
                 const pick = await input.findQuickPick('Intel oneAPI: Initialize default environment variables');
@@ -26,21 +33,15 @@ describe('DevFlow extension UI Tests', function () {
             });
 
             it('Command shows a notification with the correct text', async function () {
-                const workbench = new Workbench();
                 await workbench.executeCommand('Intel oneAPI: Initialize default environment variables');
-                await driver.sleep(1000);
-                // close the dialog after setting the environment
-                const dialog = new ModalDialog();
-                await dialog.pushButton('OK');
-
-                const notification = await driver.wait(async () => { return await getNotifications('oneAPI environment script'); }, 10000) as Notification;
+                await driver.sleep(5000);
+                const notification = await driver.wait(async () => { return await getNotifications('oneAPI environment script'); }, 70000) as Notification;
                 expect(await notification.getType()).equals(NotificationType.Info);
             });
         });
 
         describe('Intel oneAPI: Clear environment variables', function () {
             it('Quick pick contain command', async function () {
-                const workbench = new Workbench();
                 const input = await workbench.openCommandPrompt() as InputBox;
                 await input.setText('>Intel oneAPI: Clear environment variables');
                 const pick = await input.findQuickPick('Intel oneAPI: Clear environment variables');
@@ -48,11 +49,10 @@ describe('DevFlow extension UI Tests', function () {
             });
 
             it('Command shows a notification with the correct text', async function () {
-                const workbench = new Workbench();
                 await workbench.executeCommand('Intel oneAPI: Clear environment variables');
                 await driver.sleep(1000);
-                const notification = await driver.wait(async () => { return await getNotifications('oneAPI environment removed successfully.'); }, 10000) as Notification;
-                expect(await notification.getMessage()).equals('oneAPI environment removed successfully.');
+                const notification = await driver.wait(async () => { return await getNotifications('oneAPI environment has been successfully removed.'); }, 50000) as Notification;
+                expect(await notification.getMessage()).equals('oneAPI environment has been successfully removed.');
                 expect(await notification.getType()).equals(NotificationType.Info);
             });
         });
@@ -62,15 +62,11 @@ describe('DevFlow extension UI Tests', function () {
         before(async () => {
             const workbench = new Workbench();
             await workbench.executeCommand('File: Open Folder');
-            // const dialog = await DialogHandler.getOpenDialog();
-            // await dialog.selectPath(samplesPath);
-            // await dialog.confirm();
             createSetvarsConfig(samplesPath);
         });
 
         describe('Default Initialize', function () {
             it('Quick pick contain command', async function () {
-                const workbench = new Workbench();
                 const input = await workbench.openCommandPrompt() as InputBox;
                 await input.setText('>Intel oneAPI: Initialize default environment variables');
                 const pick = await input.findQuickPick('Intel oneAPI: Initialize default environment variables');
@@ -80,19 +76,15 @@ describe('DevFlow extension UI Tests', function () {
             it('Command shows a notification with the correct text', async function () {
                 const workbench = new Workbench();
                 await workbench.executeCommand('Intel oneAPI: Initialize default environment variables');
-                await driver.sleep(1000);
-                // close the dialog after setting the environment
-                const dialog = new ModalDialog();
-                await dialog.pushButton('OK');
+                await driver.sleep(5000);
 
-                const notification = await driver.wait(async () => { return await getNotifications('oneAPI environment script'); }, 10000) as Notification;
+                const notification = await driver.wait(async () => { return await getNotifications('oneAPI environment script'); }, 50000) as Notification;
                 expect(await notification.getType()).equals(NotificationType.Info);
             });
         });
 
         describe('Custom Initialize', function () {
             it('Quick pick contain command', async function () {
-                const workbench = new Workbench();
                 const input = await workbench.openCommandPrompt() as InputBox;
                 await input.setText('>Intel oneAPI: Initialize custom environment variables');
                 const pick = await input.findQuickPick('Intel oneAPI: Initialize custom environment variables');
@@ -100,7 +92,6 @@ describe('DevFlow extension UI Tests', function () {
             });
 
             it('Command shows a notification with the correct text', async function () {
-                const workbench = new Workbench();
                 await workbench.executeCommand('Intel oneAPI: Clear environment variables');
                 await workbench.executeCommand('Intel oneAPI: Initialize custom environment variables');
                 await driver.sleep(1000);
@@ -111,7 +102,6 @@ describe('DevFlow extension UI Tests', function () {
         });
         describe('Intel oneAPI: Switch environment', function () {
             it('Quick pick contain command', async function () {
-                const workbench = new Workbench();
                 const input = await workbench.openCommandPrompt() as InputBox;
                 await input.setText('>Intel oneAPI: Switch environment');
                 const pick = await input.findQuickPick('Intel oneAPI: Switch environment');
@@ -119,19 +109,17 @@ describe('DevFlow extension UI Tests', function () {
             });
 
             it('Switching directory shows a notification with the correct text', async function () {
-                const workbench = new Workbench();
                 const input = await workbench.openCommandPrompt() as InputBox;
                 await input.setText('>Intel oneAPI: Switch environment');
                 await input.selectQuickPick('Intel oneAPI: Switch environment');
-                await driver.sleep(1000);
-                const notification = await driver.wait(async () => { return await getNotifications('Nothing to switch!'); }, 10000) as Notification;
+                await driver.sleep(5000);
+                const notification = await driver.wait(async () => { return await getNotifications('Alternate environment is not available. Open settings and search for SETVARS_CONFIG to specify the path to your custom configuration file.'); }, 50000) as Notification;
                 expect(await notification.getType()).equals(NotificationType.Info);
             });
         });
 
         describe('Intel oneAPI: Clear environment variables', function () {
             it('Quick pick contain command', async function () {
-                const workbench = new Workbench();
                 const input = await workbench.openCommandPrompt() as InputBox;
                 await input.setText('>Intel oneAPI: Clear environment variables');
                 const pick = await input.findQuickPick('Intel oneAPI: Clear environment variables');
@@ -139,11 +127,10 @@ describe('DevFlow extension UI Tests', function () {
             });
 
             it('Command shows a notification with the correct text', async function () {
-                const workbench = new Workbench();
                 await workbench.executeCommand('Intel oneAPI: Clear environment variables');
                 await driver.sleep(1000);
-                const notification = await driver.wait(async () => { return await getNotifications('Undefined environment'); }, 10000) as Notification;
-                expect(await notification.getMessage()).equals('Environment variables have not been configured previously and cannot be cleared.');
+                const notification = await driver.wait(async () => { return await getNotifications('Environment variables were not configured, so environment is not set. No further action needed.'); }, 50000) as Notification;
+                expect(await notification.getMessage()).equals('Environment variables were not configured, so environment is not set. No further action needed.');
                 expect(await notification.getType()).equals(NotificationType.Info);
             });
         });
@@ -175,5 +162,4 @@ function createSetvarsConfig(path: string): void {
     const settingsJsonContent = `{\n"SETVARS_CONFIG":"${configPath}"\n}`;
     const settingsJsonPath = join(vscodeFolderPath, 'settings.json');
     writeFileSync(settingsJsonPath, settingsJsonContent);
-
 }
